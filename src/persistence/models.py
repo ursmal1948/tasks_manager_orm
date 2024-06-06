a7b702d565901ab1964444a033df201a41a80877
ursmal1948 <ula.malin35@gmail.com>
2024-06-07 01:29:18 +0200

    update in models entities

--- src/persistence/models.py
+++ src/persistence/models.py
@@ -1,3 +1,5 @@
+import enum
+from datetime import datetime
 from sqlalchemy import (
     ForeignKey,
     String,
@@ -12,6 +14,7 @@ from sqlalchemy.orm import (DeclarativeBase,
                             relationship,
                             sessionmaker
                             )
+from enum import Enum
 
 
 class Base(DeclarativeBase):
@@ -23,6 +26,8 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(30), nullable=False)
 
     projects: Mapped[list['Project']] = relationship('Project', back_populates='user')
     comments: Mapped[list['Comment']] = relationship('Comment', back_populates='user')
@@ -40,6 +45,7 @@ class Project(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
+    # description: Mapped[str] = mapped_column(String(30), nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
     user: Mapped[User] = relationship('User', back_populates='projects')
@@ -52,11 +58,18 @@ class Project(Base):
         return str(self)
 
 
+class TaskStatus(Enum):
+    NEW = 0
+    IN_PROGRESS = 1
+    COMPLETED = 2
+
+
 class Task(Base):
     __tablename__ = 'tasks'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(30), nullable=False)
+    status: Mapped[TaskStatus] = mapped_column(default=TaskStatus.NEW)
     project_id: Mapped[int] = mapped_column(ForeignKey('projects.id'))
 
     project: Mapped[Project] = relationship('Project', back_populates='tasks')
@@ -64,7 +77,7 @@ class Task(Base):
     task_histories: Mapped[list['TaskHistory']] = relationship('TaskHistory', back_populates='task')
 
     def __str__(self) -> str:
-        return f'ID: {self.id} Task: {self.title}'
+        return f'ID: {self.id} Task: {self.title} Staus: {self.status.name}'
 
     def __repr__(self):
         return str(self)
@@ -75,6 +88,7 @@ class Comment(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     content: Mapped[str] = mapped_column(String(30), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     task_id: Mapped[int] = mapped_column(ForeignKey('tasks.id'))
 
@@ -92,7 +106,7 @@ class TaskHistory(Base):
     __tablename__ = 'task_histories'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    content: Mapped[str] = mapped_column(String(30), nullable=False)
+    description_change: Mapped[str] = mapped_column(String(30), nullable=False)
     task_id: Mapped[int] = mapped_column(ForeignKey('tasks.id'))
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
@@ -100,7 +114,16 @@ class TaskHistory(Base):
     user: Mapped[User] = relationship('User', back_populates='task_histories')
 
     def __str__(self) -> str:
-        return f'ID: {self.id} Task History: {self.content}'
+        return f'ID: {self.id} Task History: {self.description_change}'
 
     def __repr__(self):
         return str(self)
+#
+#
+# def main() -> None:
+#     metadata = Base.metadata
+#     metadata.create_all(engine)
+#
+#
+# if __name__ == '__main__':
+#     main()
