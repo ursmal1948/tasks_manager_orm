0593ef8f0f8517c89f988c0ccddfdaf0faabc123
ursmal1948 <ula.malin35@gmail.com>
2024-07-12 03:05:50 +0200

    default_conf fle

---
+++ tasks/app/service/configuration.py
@@ -0,0 +1,15 @@
+from app.db.repository import project_repository, user_repository, task_repository, comment_repository
+from app.service.users_service import UsersService
+from app.service.projects_service import ProjectsService
+from app.service.users_with_projects import UsersWithProjectsService
+from app.service.projects_with_tasks_service import ProjectsWithTasksService
+from app.service.tasks_service import TasksService
+
+from app.service.comments_service import CommentsService
+
+users_service = UsersService(user_repository)
+projects_service = ProjectsService(project_repository)
+users_with_projects_service = UsersWithProjectsService(user_repository, project_repository)
+projects_with_tasks_service = ProjectsWithTasksService(project_repository, task_repository, user_repository)
+tasks_service = TasksService(task_repository)
+comments_service = CommentsService(comment_repository, user_repository, task_repository)
