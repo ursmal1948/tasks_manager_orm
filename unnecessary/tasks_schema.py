ed85ee6a21a9975722cface3f4a85b67f7a568cd
ursmal1948 <ula.malin35@gmail.com>
2024-07-12 03:13:56 +0200

    deleted package

--- unnecessary/tasks_schema.py
+++
@@ -1,8 +0,0 @@
-add_task_schema = {
-    "type": "object",
-    "properties": {
-        "status": {"type": "string", "enum": ["NEW", "IN_PROGRESS", "COMPLETED"], "default": "NEW"},
-        "project_id": {"type": "integer"},
-    },
-    "required": ["project_id"],
-}
