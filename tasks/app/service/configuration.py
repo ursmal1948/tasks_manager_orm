08307ad32325a0928b83dadd740d4fd1991e5e7e
ursmal1948 <ula.malin35@gmail.com>
2024-07-31 23:24:10 +0200

    tasks histories service added

--- tasks/app/service/configuration.py
+++ tasks/app/service/configuration.py
@@ -1,11 +1,17 @@
-from app.db.repository import project_repository, user_repository, task_repository, comment_repository
+from app.db.repository import (
+    project_repository,
+    user_repository,
+    task_repository,
+    comment_repository,
+    task_history_repository
+)
 from app.service.users_service import UsersService
 from app.service.projects_service import ProjectsService
 from app.service.users_with_projects import UsersWithProjectsService
 from app.service.projects_with_tasks_service import ProjectsWithTasksService
 from app.service.tasks_service import TasksService
-
 from app.service.comments_service import CommentsService
+from app.service.task_histories_service import TaskHistoriesService
 
 users_service = UsersService(user_repository)
 projects_service = ProjectsService(project_repository)
@@ -13,3 +19,4 @@ users_with_projects_service = UsersWithProjectsService(user_repository, project_
 projects_with_tasks_service = ProjectsWithTasksService(project_repository, task_repository, user_repository)
 tasks_service = TasksService(task_repository)
 comments_service = CommentsService(comment_repository, user_repository, task_repository)
+task_histories_service = TaskHistoriesService(task_repository, user_repository,task_history_repository)
