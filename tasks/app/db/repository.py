52e340e5e018138945c606f0f5887ceaba659048
ursmal1948 <ula.malin35@gmail.com>
2024-08-04 01:37:11 +0200

    changed some of the methods in repositories in favour for newer sqlalchemy version and its possibilty of making queries (more sql way)

--- tasks/app/db/repository.py
+++ tasks/app/db/repository.py
@@ -1,16 +1,19 @@
 from abc import ABC, abstractmethod
-from app.db.configuration import sa
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select, delete
 from typing import Any
+import logging
+
+from app.db.configuration import sa
 from app.db.entity import (
     UserEntity,
     ProjectEntity,
     TaskEntity,
     CommentEntity,
-    TaskStatus)
-import logging
-from sqlalchemy import select
+    TaskStatus,
+    TaskHistoryEntity
+)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -42,6 +45,8 @@ class CrudRepository[T](ABC):
         pass
 
 
+#
+#
 class CrudRepositoryORM[T:sa.Model](CrudRepository):
     def __init__(self, db: SQLAlchemy, entity_type: Any) -> None:
         self.sa = db
@@ -57,20 +62,23 @@ class CrudRepositoryORM[T:sa.Model](CrudRepository):
 
     def find_by_id(self, entity_id: int) -> T | None:
         stmt = select(self.entity_type).filter_by(id=entity_id)
-        return self.sa.session.execute(stmt).scalars().first()
+        return self.sa.session.execute(stmt).scalar_one_or_none()
 
     def find_all(self) -> list[T]:
-        return self.sa.session.query(self.entity_type).all()
+        stmt = select(self.entity_type)
+        return self.sa.session.execute(stmt).scalars().all()
 
     def delete_by_id(self, entity_id: int) -> None:
         entity = self.find_by_id(entity_id)
-        if entity:
-            self.sa.session.delete(entity)
-            self.sa.session.commit()
+        # if entity:
+        self.sa.session.delete(entity)
+        self.sa.session.commit()
 
     def delete_all(self) -> None:
-        self.sa.session.query(self.entity_type).delete()
-        self.sa.session.commit()
+        entities = self.find_all()
+        for e in entities:
+            self.sa.session.delete(e)
+            self.sa.session.commit()
 
 
 class UserRepository(CrudRepositoryORM[UserEntity]):
@@ -81,34 +89,35 @@ class UserRepository(CrudRepositoryORM[UserEntity]):
         return self.entity_type.query.filter_by(name=name).first()
 
     def find_email_by_id(self, user_id: int) -> str:
-        return self.entity_type.query.filter_by(id=user_id).first().email
+        stmt = select(self.entity_type).filter_by(id=user_id)
+        return self.sa.session.execute(stmt).scalars().first().email
 
     def find_all_projects_for_user(self, user_id: int) -> list[ProjectEntity]:
         return self.find_by_id(user_id).projects
-        # if user:
-        #     # user.p
-        #     return user.projects
 
     def find_by_email(self, email: str) -> UserEntity | None:
-        return self.entity_type.query.filter_by(email=email).first()
+        stmt = select(UserEntity).filter_by(email=email)
+        return self.sa.session.execute(stmt).scalar_one_or_none()
+
+    def find_last_added_user(self) -> UserEntity | None:
+        stmt = select(self.entity_type).order_by(UserEntity.id.desc()).limit(1)
+        return self.sa.session.execute(stmt).scalars().first()
 
 
 class ProjectRepository(CrudRepositoryORM[UserEntity]):
     def __init__(self, db: SQLAlchemy):
         super().__init__(db, ProjectEntity)
 
+    # nie sprwadzilam tego jeszce.
     def find_by_name(self, name: str) -> UserEntity | None:
-        return self.entity_type.query.filter_by(name=name).first()
+        stmt = select(ProjectEntity).filter_by(name=name)
+        return self.sa.session.execute(stmt).scalar_one_or_none()
 
     def find_all_tasks_for_project(self, project_id: int) -> list[TaskEntity]:
         return self.find_by_id(project_id).tasks
 
     def find_user_id_by_project_id(self, project_id: int) -> str:
         return self.find_by_id(project_id).user_id
-
-
-# def find_user_id_for_project(self,project_id:int)->int:
-#     return self.find_by_id(project_id).user_id
 
 
 class TaskRepository(CrudRepositoryORM[TaskEntity]):
@@ -118,15 +127,15 @@ class TaskRepository(CrudRepositoryORM[TaskEntity]):
     def find_task_project(self, task_id: int) -> ProjectEntity:
         return self.find_by_id(task_id).project
 
-    def find_by_title(self, title: str) -> ProjectEntity:
-        return self.entity_type.query.filter_by(title=title).first()
-
     def find_tasks_by_status(self, status: TaskStatus) -> list[TaskEntity]:
-        return self.entity_type.query.filter_by(status=status.name).all()
+        stmt = select(TaskEntity).filter_by(status=status.name)
+        return self.sa.session.execute(stmt).scalars().all()
 
-    # id usera ktory ma taska.
     def find_project_id_by_task_id(self, task_id: int) -> int:
         return self.find_by_id(task_id).project_id
+
+    def find_task_histories_for_task(self, task_id: int) -> list[TaskHistoryEntity]:
+        return self.find_by_id(task_id).task_histories
 
 
 class CommentRepository(CrudRepositoryORM[CommentEntity]):
@@ -139,7 +148,13 @@ class CommentRepository(CrudRepositoryORM[CommentEntity]):
         return self.sa.session.execute(stmt).scalars().first()
 
 
+class TaskHistoryRepository(CrudRepositoryORM[TaskHistoryEntity]):
+    def __init__(self, db: SQLAlchemy):
+        super().__init__(db, TaskHistoryEntity)
+
+
 user_repository = UserRepository(sa)
 project_repository = ProjectRepository(sa)
 task_repository = TaskRepository(sa)
 comment_repository = CommentRepository(sa)
+task_history_repository = TaskHistoryRepository(sa)
